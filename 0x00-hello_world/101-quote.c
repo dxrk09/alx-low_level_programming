610a09ecba2296c6c37b3dc8d91e529aa8186458
Dxrk <118543407+dxrk09@users.noreply.github.com>
2023-09-08 02:16:24 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <unistd.h>
-
+/**
+ * main - A c program that prints a line to the standard error
+ * Return: one
+ */
 int main(void)
 {
 write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
