33374cbfa98ad56a76611bdcfddc22380b45aae5
Dxrk <118543407+dxrk09@users.noreply.github.com>
2023-09-13 03:12:02 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,9 @@
 #include "main.h"
 
 /**
- * main -Entry point
- * Description: 'printing 0. _putchar'
- * Return: Always 0 (Success)
-*/
-
+ * main - Print _putchar
+ * Return: Always 0
+ */
 int main(void)
 {
 	_putchar('_');
@@ -17,6 +15,5 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-
 	return (0);
 }
