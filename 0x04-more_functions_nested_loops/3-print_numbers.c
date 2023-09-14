35cd4fcba65e0a6678bf8086dd022bf62f7dc824
Dxrk <118543407+dxrk09@users.noreply.github.com>
2023-09-14 22:52:15 +0100

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
-*print_numbers - print 0123456789
-*Return: void
-*/
-
+ * print_numbers - function that prints the numbers, from 0 to 9.
+ * Decription: printing number fro 0 to 9 using _putchar fun.
+ * Return: Always 0 (Success).
+ */
 
 void print_numbers(void)
 {
-        char c;
+	int i;
 
-        for (c = '0'; c <= '9'; c++)        
-        {
-         _putchar(c); 
-        } 
-        _putchar('\n')
+	for (i = 48 ; i <= 57 ; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
 }
