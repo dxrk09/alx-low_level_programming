da583bedc016ec34a5601e3e369acbc63f73cba0
Dxrk <118543407+dxrk09@users.noreply.github.com>
2023-09-19 01:21:24 +0100

    Create 0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - reset_to_98
+ *
+ * Return: Always 0.
+ */
+
+void reset_to_98(int *n) {
+    *n = 98;
+}
+
+int main() {
+    int num = 42;
+    printf("Before reset: %d\n", num);
+    
+    // Call the function to reset num to 98
+    reset_to_98(&num);
+    
+    printf("After reset: %d\n", num);
+    
+    return 0;
+}
