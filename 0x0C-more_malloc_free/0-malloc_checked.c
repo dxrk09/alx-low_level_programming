6bb99002edb75c3176dff74dd3dbd365dfc61a1d
Dxrk <118543407+dxrk09@users.noreply.github.com>
2023-10-08 21:23:51 +0100

    Update 0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * malloc_checked - cause normal process termination with a status value of 98
+ * malloc_checked - allocates memory using malloc
  *
  * @b: allocated memory
  *
